c8c1fb70b3507185cf3f168cc29d2e81c98cfacd
leesangmin4533 <kanura@naver.com>
2025-06-26 14:12:18 +0900

    Update login path and scripts

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import json
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 def load_login_url():
@@ -15,6 +16,14 @@ def main():
     url = load_login_url()
     driver = webdriver.Chrome()
     driver.get(url)
+    # Automatically fill the ID field using the latest XPath configuration.
+    xpath_path = "/html/body/div/div/div/div[1]/div/div/div[1]/div/div/div/div[1]/div/div[5]/input"
+    try:
+        id_input = driver.find_element(By.XPATH, xpath_path)
+        id_input.clear()
+        id_input.send_keys("46513")
+    except Exception:
+        pass
     input("Login screen displayed. Press Enter to exit...")
     driver.quit()
 
