966d5f9dfcc64ac47b077f4b68bb9acb34302bf6
leesangmin4533 <kanura@naver.com>
2025-06-28 07:58:43 +0900

    Refactor navigation code into module

---
+++ modules/sales_analysis/navigate_to_mid_category.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+def navigate_to_mid_category_sales(driver):
+    """Navigate to the '중분류별 매출 구성' page under sales analysis."""
+    print("🟡 매출분석 메뉴 클릭")
+    driver.find_element(By.XPATH, '//*[@id="mainframe.HFrameSet00.VFrameSet00.TopFrame.form.div_topMenu.form.STMB000_M0:icontext"]/div').click()
+    time.sleep(1)
+
+    print("🟡 중분류 메뉴 등장 대기")
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="mainframe.HFrameSet00.VFrameSet00.TopFrame.form.pdiv_topMenu_STMB000_M0.form.STMB011_M0:text"]'))
+    )
+    time.sleep(0.5)
+
+    print("🟢 중분류별 매출 구성비 클릭")
+    driver.find_element(By.XPATH, '//*[@id="mainframe.HFrameSet00.VFrameSet00.TopFrame.form.pdiv_topMenu_STMB000_M0.form.STMB011_M0:text"]').click()
+    time.sleep(2)
+
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="mainframe.HFrameSet00.VFrameSet00.FrameSet.STMB011_M0.form.div_workForm.form.div2.form.gdList.body.gridrow_0.cell_0_0"]'))
+    )
