c0e23f9e9aef26ddfdf21b43c5babf3ab8c4cc12
leesangmin4533 <kanura@naver.com>
2025-07-02 09:55:05 +0900

    Merge navigation into scroll module and call it

--- modules/sales_analysis/arrow_fallback_scroll.py
+++ modules/sales_analysis/arrow_fallback_scroll.py
@@ -1,11 +1,53 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import re
 import os
 
+from log_util import create_logger
 from .grid_click_logger import log_detail
+
+log = create_logger("navigation")
+
+
+def navigate_to_mid_category_sales(driver):
+    """Navigate to the '중분류별 매출 구성' page under sales analysis."""
+    log("open_menu", "실행", "매출분석 메뉴 클릭")
+    driver.find_element(
+        By.XPATH,
+        '//*[@id="mainframe.HFrameSet00.VFrameSet00.TopFrame.form.div_topMenu.form.STMB000_M0"]',
+    ).click()
+    time.sleep(1)
+
+    log("wait_mid_menu", "실행", "중분류 메뉴 등장 대기")
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(
+            (
+                By.XPATH,
+                '//*[@id="mainframe.HFrameSet00.VFrameSet00.TopFrame.form.pdiv_topMenu_STMB000_M0.form.STMB011_M0"]',
+            )
+        )
+    )
+    time.sleep(0.5)
+
+    log("click_mid_sales", "실행", "중분류별 매출 구성비 클릭")
+    driver.find_element(
+        By.XPATH,
+        '//*[@id="mainframe.HFrameSet00.VFrameSet00.TopFrame.form.pdiv_topMenu_STMB000_M0.form.STMB011_M0"]',
+    ).click()
+    time.sleep(2)
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(
+            (
+                By.XPATH,
+                '//*[@id="mainframe.HFrameSet00.VFrameSet00.FrameSet.STMB011_M0.form.WorkFrame.form.grd_msg.body.gridrow_0.cell_0_0"]',
+            )
+        )
+    )
 
 
 def find_cell_under_mainframe(driver, depth: int = 6):
