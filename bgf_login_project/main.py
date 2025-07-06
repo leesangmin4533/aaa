3fdce5d0b1fbfc5b7475c31302e7fce32c798f5b
leesangmin4533 <kanura@naver.com>
2025-07-06 16:39:56 +0900

    Make package importable and fix parse_mix_ratio_data

--- bgf_login_project/main.py
+++ bgf_login_project/main.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from login.login_bgf import login_bgf
-from analysis import (
+from bgf_login_project.login.login_bgf import login_bgf
+from bgf_login_project.analysis import (
     go_to_category_mix_ratio,
     parse_mix_ratio_data,
     extract_code_details_with_button_scroll,
