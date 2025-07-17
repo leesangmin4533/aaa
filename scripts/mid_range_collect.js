f3d2956ae5ac3731a49d5dccfd61e9016716b475
leesangmin4533 <kanura@naver.com>
2025-07-17 12:17:08 +0900

    Remove mid_range_collect.js and update docs

--- scripts/mid_range_collect.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const start = window.__MID_RANGE_START__ || null;
-  const end = window.__MID_RANGE_END__ || null;
-  if (typeof window.collectMidProducts === 'function') {
-    window.collectMidProducts(start, end);
-  } else {
-    console.warn('collectMidProducts 함수가 정의되어 있지 않습니다. auto_collect_mid_products.js를 먼저 실행하세요.');
-  }
-})();
