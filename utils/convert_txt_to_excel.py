0130e679c7edf46092c4d7525e5fa65c8c042968
leesangmin4533 <kanura@naver.com>
2025-07-19 23:36:46 +0900

    Update convert_txt_to_excel.py

--- utils/convert_txt_to_excel.py
+++ utils/convert_txt_to_excel.py
@@ -6,12 +6,12 @@ import sys
 import pandas as pd
 
 if __package__:
-    from .log_util import create_logger
+    from .log_util import get_logger
 else:  # pragma: no cover - fallback when executed directly
     sys.path.append(str(Path(__file__).resolve().parent))
     from log_util import create_logger
 
-log = create_logger("convert_txt_to_excel")
+log = get_logger(__name__)
 
 
 def convert_txt_to_excel(
