4122d870dff9f56ae9b9566be34f4eaf1fe6dd45
leesangmin4533 <kanura@naver.com>
2025-07-22 21:45:33 +0900

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,12 +1,11 @@
 """Utility package."""
 from .db_util import init_db, write_sales_data
 from .txt_parser import parse_txt
-from .js_util import load_collect_past7days, execute_collect_single_day_data
+from .js_util import execute_collect_single_day_data
 
 __all__ = [
     "init_db",
     "write_sales_data",
     "parse_txt",
-    "load_collect_past7days",
     "execute_collect_single_day_data",
 ]
